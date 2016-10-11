79a79c763bb36962ca560d04b1cf386071f1a75d
rajunigadi <raju.nigadi@gmail.com>
2016-10-11 11:31:21 +0530

    Added Location & Internet permission.

--- app/src/main/java/com/vtechsofts/googlempas/MainActivity.java
+++ app/src/main/java/com/vtechsofts/googlempas/MainActivity.java
@@ -2,13 +2,10 @@ package com.vtechsofts.googlempas;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -16,14 +13,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnCameraMoveStartedListener,
         GoogleMap.OnCameraMoveListener, GoogleMap.OnCameraIdleListener {
 
     private GoogleMap mMap;
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +39,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        latitude = 12.9715987;
+        longitude = 77.5945627;
+
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng latLng = new LatLng(12.9715987, 77.5945627);
+        LatLng latLng = new LatLng(latitude, longitude);
         mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in Bengaluru"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
@@ -57,13 +60,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             googleMap.getUiSettings().setMyLocationButtonEnabled(false);
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                     && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 return;
             }
             googleMap.setOnCameraMoveStartedListener(this);
@@ -86,4 +82,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onCameraIdle() {
         Log.e("raja", "onCameraIdle");
     }
+
 }
